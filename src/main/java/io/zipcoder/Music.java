e105cbfa27d5aeb01a1e748eca8fb00a429752b9
deltagphys <cty.jgates@gmail.com>
2019-10-22 15:26:32 -0400

    all tests passing

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -1,5 +1,8 @@
 package io.zipcoder;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Music {
 
     private String[] playList;
@@ -9,6 +12,14 @@ public class Music {
     }
 
     public Integer selection(Integer startIndex, String selection){
-        return null;
+        Integer length = this.playList.length;
+        for (int i = 1; i < length; i++) {
+
+            if (playList[(startIndex + i)%length].equals(selection) || playList[(length + startIndex - i)%length].equals(selection)) {
+                return i;
+            }
+        }
+
+        return 0;
     }
 }
